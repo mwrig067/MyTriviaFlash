6d6aa8900702fdedf702a837893921e8e2b9ee75
Mathew Wright <mwrig067@fiu.edu>
2022-10-02 16:30:23 -0500

    Adding button for new view

--- app/src/main/java/com/example/mytriviaflash/MainActivity.kt
+++ app/src/main/java/com/example/mytriviaflash/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mytriviaflash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +15,17 @@ class MainActivity : AppCompatActivity() {
 
         val flashCardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashCardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val addButton = findViewById<ImageView>(R.id.icon_add)
+
+
 
         flashCardQuestion.setOnClickListener {
             flashCardQuestion.visibility = View.INVISIBLE
             flashCardAnswer.visibility = View.VISIBLE
+        }
+        addButton.setOnClickListener {
+            val intent = Intent (this,AddCardActivity::class.java)
+            startActivity(intent)
         }
 
     }
